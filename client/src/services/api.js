28d97c6aae4f27ce846d140df5b2ae70cffaf2c5
moti gabay <motigabay18@gmail.com>
2026-04-07 14:07:42 +0300

    add base url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/tasks",
+  baseURL: import.meta.env.VITE_API_URL || "/api/tasks",
   headers: { "Content-Type": "application/json" },
 });
 
--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ const tasksRouter = require("./routes/tasks");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
 app.use(express.json());
 
 app.use("/api/tasks", tasksRouter);
